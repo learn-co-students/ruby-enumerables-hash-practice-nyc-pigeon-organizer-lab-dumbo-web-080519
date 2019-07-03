674010634accd9db4192dec842f2fb16642c5b31
Austin Trow <austintrow@gmail.com>
2019-07-03 02:46:24 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized = {}
+  
+  data.keys.each do |key|
+    data[key].keys.each do |value|
+      data[key][value].each do |name|
+        if !organized.has_key?(name)
+          organized[name] = {}
+        end
+        if !organized[name].has_key?(key)
+          organized[name][key] = [value.to_s]
+        else
+          organized[name][key].push(value.to_s)
+        end
+      end
+    end
+  end
+  
+  organized
 end
