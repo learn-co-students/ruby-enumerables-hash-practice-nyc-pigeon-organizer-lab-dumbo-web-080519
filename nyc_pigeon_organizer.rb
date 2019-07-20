45b44c7d98628072111186600dc1deb11fc8c653
Kimberly Bone <k_bone@uncg.edu>
2019-07-20 06:27:35 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,40 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  new = {}
+  data.each do |key, value|
+    value.each do |k,v|
+      v.each do |name|
+        new[name] = {:color => [], :gender => [], :lives => []}
+      end 
+    end 
+  end 
+  x = new.keys
+  data[:color].each do |col, name|
+    name.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          new[item][:color] << col.to_s
+        end
+      end
+    end
+  end
+  data[:gender].each do |gender, type|
+    type.each do |name|
+      x.each do |item|
+        if name === item
+          new[item][:gender] << gender.to_s
+        end
+      end
+    end
+  end
+  data[:lives].each do |lives, loc|
+    loc.each do |name|
+      x.each do |item|
+        if name === item
+          new[item][:lives] << lives.to_s
+        end
+      end
+    end
+  end
+  return new
+end 
+
