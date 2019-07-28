bf1ed07578b6f73894a3f96933955e8e9e346417
Avijit Lodh <avijitklodh@gmail.com>
2019-07-28 22:46:51 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,42 +1,41 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
- final = {}
+def nyc_pigeon_organizer (data)
+  organized_pigeon = {}
 
-  data.each do |first_level, all_other|
-    all_other.each do |category, array|
-      array.each do |name|
-        final[name] = {:color => [], :gender => [], :lives => []}
+  data.each do |key, value|
+    value.each do |key_2, array|
+      array.each do |names|
+        organized_pigeon[names] = {:color => [], :gender => [], :lives => []}
       end 
     end 
   end 
-  x = final.keys
-  data[:color].each do |bird_color, name|
-    name.each do |bird_name|
+  x = organized_pigeon.keys
+  data[:color].each do |color, color_name|
+    color_name.each do |bird_name|
       x.each do |item|
         if bird_name === item
-          final[item][:color] << bird_color.to_s
+          organized_pigeon[item][:color] << color.to_s
         end 
       end 
     end 
   end 
-  data[:gender].each do |gender, type|
-    type.each do |bird_name|
+  data[:gender].each do |gender, sex|
+    sex.each do |bird_name|
       x.each do |item|
         if bird_name === item
-          final[item][:gender] << gender.to_s
+          organized_pigeon[item][:gender] << gender.to_s
         end 
       end 
     end 
   end 
-  data[:lives].each do |location, name|
+  data[:lives].each do |place, name|
     name.each do |bird_name|
       x.each do |item|
         if bird_name === item
-          final[item][:lives] << location
+          organized_pigeon[item][:lives] << place
         end 
       end 
     end 
   end 
   
-  return final 
-end 
+  return organized_pigeon 
+end
